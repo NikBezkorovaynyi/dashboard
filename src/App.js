2e11ad35fbec02393fba1364ad27533667a71403
NikBezkorovaynyi <kolyabeskoro26@gmail.com>
2023-02-28 23:31:22 +0200

    rename

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { ColorModeContext, useMode} from './theme';
 import { CssBaseline, ThemeProvider} from "@mui/material";
 import { Routes, Route } from "react-router-dom";
-import Topbar from "./scenes/global/Topbar";
-import Sidebar from "./scenes/global/Sidebar";
-import Dashboard from "./scenes/dashboard";
-import Team from "./scenes/team";
+import Topbar from "./pages/global/Topbar";
+import Sidebar from "./pages/global/Sidebar";
+import Dashboard from "./pages/dashboard";
+import Team from "./pages/team";
 // import Invoices from "./scenes/invoices";
 // import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
